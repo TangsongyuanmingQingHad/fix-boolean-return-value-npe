ea9bcebc996c16ccaad5b74a6e88e1bb96d09cbb
TangsongyuanmingQingHAD <had13625428701@aliyun.com>
2019-12-09 09:55:42 +0800

    应该在构造方法中修改，get方法只读

--- src/main/java/com/github/hcsp/datatype/Light.java
+++ src/main/java/com/github/hcsp/datatype/Light.java
@@ -8,14 +8,14 @@ public class Light {
     Boolean on;
 
     public Light(Boolean on) {
+        if(on == null) {
+            on = false;
+        }
         this.on = on;
     }
 
     // 当灯亮时返回true，灭和未知状态返回false
     public boolean isOn() {
-        if(on == null) {
-            on = false;
-        }
         return on;
     }
 
